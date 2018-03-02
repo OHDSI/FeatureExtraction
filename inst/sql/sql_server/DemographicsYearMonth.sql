98684e63e687c42b88be9fb2d460a30a96c000eb
schuemie <schuemie@ohdsi.org>
2018-03-02 07:05:43 +0100

    Correcting typo. Fixes #48

--- inst/sql/sql_server/DemographicsYearMonth.sql
+++ inst/sql/sql_server/DemographicsYearMonth.sql
@@ -51,7 +51,7 @@ INSERT INTO #analysis_ref (
 	)
 SELECT @analysis_id AS analysis_id,
 	CAST('@analysis_name' AS VARCHAR(512)) AS analysis_name,
-	CAST('domain_id' AS VARCHAR(20)) AS domain_id,
+	CAST('@domain_id' AS VARCHAR(20)) AS domain_id,
 {!@temporal} ? {
 	CAST(NULL AS INT) AS start_day,
 	CAST(NULL AS INT) AS end_day,
