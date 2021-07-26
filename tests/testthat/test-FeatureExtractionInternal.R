131425e3e226395b1364e7025cd5834a7afc5f50
Anthony Sena <asena5@its.jnj.com>
2021-07-26 16:32:54 -0400

    Update maven config, JAR file and restore internal checksum test

--- tests/testthat/test-FeatureExtractionInternal.R
+++ tests/testthat/test-FeatureExtractionInternal.R
@@ -2,11 +2,11 @@
 # library(testthat); library(FeatureExtraction)
 # covr::file_report(covr::file_coverage("R/FeatureExtraction.R", "tests/testthat/test-FeatureExtractionInternal.R"))
 
-#test_that("Test .onLoad()", {
-#  expect_silent(
-#    FeatureExtraction:::.onLoad(libname = "FeatureExtraction", pkgname = "FeatureExtraction")
-#  )
-#})
+test_that("Test .onLoad()", {
+ expect_silent(
+   FeatureExtraction:::.onLoad(libname = "FeatureExtraction", pkgname = "FeatureExtraction")
+ )
+})
 
 test_that("Test JSON functions", {
   expectedToJsonResult <- "{\"id\":\"1\"}"
