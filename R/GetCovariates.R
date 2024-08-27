432261bf4f521848cd106070511330a634fd06ff
Ger Inberg <ginberg@gmail.com>
2024-08-27 16:29:59 +0200

    doc

--- R/GetCovariates.R
+++ R/GetCovariates.R
@@ -33,7 +33,7 @@
 #'                               \code{connect} function in the \code{DatabaseConnector} package.
 #'                               Either the \code{connection} or \code{connectionDetails} argument
 #'                               should be specified.
-#' @param oracleTempSchema       A schema where temp tables can be created in Oracle.
+#' @param oracleTempSchema       DEPRECATED: use \code{tempEmulationSchema} instead.
 #' @param cdmDatabaseSchema      The name of the database schema that contains the OMOP CDM instance.
 #'                               Requires read permissions to this database. On SQL Server, this should
 #'                               specify both the database and the schema, so for example
