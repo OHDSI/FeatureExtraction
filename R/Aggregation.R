e7ae1cb3e187ef016a61d530db37aea4cd78e6f6
Ger Inberg <ginberg@gmail.com>
2025-05-08 07:06:01 +0200

    #303

--- R/Aggregation.R
+++ R/Aggregation.R
@@ -58,10 +58,7 @@ aggregateCovariates <- function(covariateData) {
   populationSize <- attr(covariateData, "metaData")$populationSize
 
   # tmp rename rowId var
-  andromedaVersion <- utils::packageVersion("Andromeda")
-  if (andromedaVersion >= "1.0.0") {
-    covariateData$covariates <- covariateData$covariates %>% dplyr::rename(rowIdTemp = .data$rowId)
-  }
+  covariateData$covariates <- covariateData$covariates %>% dplyr::rename(rowIdTemp = .data$rowId)
 
   # Aggregate binary variables
   result$covariates <- covariateData$analysisRef %>%
